d00cf64b747173b1e4cccf756c378669079d5754
Camila Fernandez <camilafernandez.0896@gmail.com>
2023-04-01 20:41:35 -0300

    primer escritura en el repositorio

---
+++ js/function.js
@@ -0,0 +1,13 @@
+let hidetextbtn = document.getElementById('hidetextbtn');
+
+let hidetext = document.getElementById('hidetext');
+
+hidetextbtn.addEventListener('click', toggleText);
+
+function toggleText () {
+    hidetext.classList.toggleText('show');
+}
+
+
+
+
