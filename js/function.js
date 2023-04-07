0855bc9a63f95b84e3fd50be1ae3dd163aa07362
Camila Fernandez <camilafernandez.0896@gmail.com>
2023-04-07 17:43:02 -0300

    modificación del estilo del botón

--- js/function.js
+++ js/function.js
@@ -1,13 +1,9 @@
-let hidetextbtn = document.getElementById('hidetextbtn');
-
-let hidetext = document.getElementById('hidetext');
-
-hidetextbtn.addEventListener('click', toggleText);
-
-function toggleText () {
-    hidetext.classList.toggleText('show');
+document.getElementById('boton-desplegar').onclick = function () {
+    console.log("Se captura el evento click desplegar");
+    document.getElementById("habilidad").innerHTML = "Cuento con distinguida capacidad de gestión y análisis, me destaca mi eficiencia en la resolucion de problemas y trabajo en equipo.";
 }
 
-
-
-
+document.getElementById('boton-ocultar').addEventListener ('click', function () {
+    console.log("Se captura el evento click ocultar");
+    document.getElementById("habilidad").style.display = 'none';
+});
