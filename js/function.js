7974d92daf8a58e55a5e33a7755c719cf67a007f
Camila Fernandez <camilafernandez.0896@gmail.com>
2023-04-10 12:21:43 -0300

    modificación de estilos de div-habilidades

--- js/function.js
+++ js/function.js
@@ -1,9 +1,9 @@
 document.getElementById('boton-desplegar').onclick = function () {
     console.log("Se captura el evento click desplegar");
-    document.getElementById("habilidad").innerHTML = "Cuento con distinguida capacidad de gestión y análisis, me destaca mi eficiencia en la resolucion de problemas y trabajo en equipo.";
+    document.getElementById("iconos").innerHTML = "Cuento con distinguida capacidad de gestión y análisis, me destaca mi eficiencia en la resolucion de problemas y trabajo en equipo.";
 }
 
 document.getElementById('boton-ocultar').addEventListener ('click', function () {
     console.log("Se captura el evento click ocultar");
-    document.getElementById("habilidad").style.display = 'none';
+    document.getElementById("iconos").style.display = 'none';
 });
